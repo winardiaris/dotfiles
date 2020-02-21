fa157d724b865bbbc600a39319dde39442f7d461
winardiaris <ariswinardy@gmail.com>
2020-02-22 03:26:32 +0700

    update dotfiles

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -11,5 +11,3 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar
 polybar -c ~/.config/polybar/themes/winardiaris/config.ini winardiaris &
 # polybar -c ~/.config/polybar/themes/nordic/config.ini nordic &
-# polybar -c ~/.config/polybar/config.ini top &
-# polybar -c ~/.config/polybar/config.ini bottom &
